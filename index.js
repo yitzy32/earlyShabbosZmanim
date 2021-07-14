1a0fc0ab73af11aea2d05161adcb6ecf77032a8a
Yitzy Bratt <yitzybratt32@gmail.com>
2021-07-14 19:36:31 -0400

    axios

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello from the console")
+axios.get('https://www.hebcal.com/zmanim?cfg=json&geonameid=3448439&date=2021-03-23')
+    .then(function (response) {
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
