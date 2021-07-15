be169b26aee4e9dc90242c77ec4447c10b81a985
Yitzy Bratt <yitzybratt32@gmail.com>
2021-07-14 21:40:31 -0400

    console log basic times

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 axios.get('https://www.hebcal.com/zmanim?cfg=json&geonameid=3448439&date=2021-03-23')
     .then(function (response) {
         console.log(response.data);
+        let date = response.data["date"]
+        let location = response.data["location"]["name"]
+        let minchaGedola = response.data["times"]["minchaGedola"]
+        let plagHaMincha = response.data["times"]["plagHaMincha"]
+        let shkia = response.data["times"]["sunset"]
+        let tzeis50min = response.data["times"]["tzeit50min"]
+        console.log("tzeis50min", tzeis50min)
     })
     .catch(function (error) {
         console.log(error);
