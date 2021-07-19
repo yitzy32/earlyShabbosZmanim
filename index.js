8c52162668cb9300d9548b0eed5717a5f65c0a84
Yitzy Bratt <yitzybratt32@gmail.com>
2021-07-18 20:52:41 -0400

    parse date

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ axios.get('https://www.hebcal.com/zmanim?cfg=json&geonameid=3448439&date=2021-03
         let shkia = response.data["times"]["sunset"]
         let tzeis50min = response.data["times"]["tzeit50min"]
 
+        let formatedDate = moment(date, "YYYY MM DD").format("LL");
         let formatedMinchaGedola = moment(minchaGedola, "YYYY-MM-DDTHH:mm:ss").format("h:mm A")
         let formatedPlagHaMincha = moment(plagHaMincha, "YYYY-MM-DDTHH:mm:ss").format("h:mm A")
         let formatedShkia = moment(shkia, "YYYY-MM-DDTHH:mm:ss").format("h:mm A")
         let formatedTzeis50min = moment(tzeis50min, "YYYY-MM-DDTHH:mm:ss").format("h:mm A")
-        console.log("Mincha Gedola", formatedMinchaGedola, "Plag", formatedPlagHaMincha, "Shkia", formatedShkia, "Tzeis 50 Min", formatedTzeis50min)
+        console.log("Date", formatedDate, "Mincha Gedola", formatedMinchaGedola, "Plag", formatedPlagHaMincha, "Shkia", formatedShkia, "Tzeis 50 Min", formatedTzeis50min)
     })
     .catch(function (error) {
         console.log(error);
