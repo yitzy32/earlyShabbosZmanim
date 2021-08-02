80bdd53cd44a7fbe64e897654a7785c38653ad26
Yitzy Bratt <yitzybratt32@gmail.com>
2021-08-01 22:32:37 -0400

    move code inside getInputValue function. make zipcode part of url dynamic

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
 function getInputValue() {
     let inputVal = document.getElementById("zipcode").value;
     console.log(inputVal);
-}
+    let thisFridaySlashes = moment().endOf('week').subtract(1, 'day').format("L");
+    let thisFridayDashes = ""
 
-let thisFridaySlashes = moment().endOf('week').subtract(1, 'day').format("L");
-let thisFridayDashes = ""
+    for (let index = 0; index < 4; index++) {
 
-for (let index = 0; index < 4; index++) {
+        splitDay = thisFridaySlashes.split("/")
+        thisFridayDashes += splitDay[2]
+        thisFridayDashes += "-"
+        thisFridayDashes += splitDay[0]
+        thisFridayDashes += "-"
+        thisFridayDashes += splitDay[1]
 
-    splitDay = thisFridaySlashes.split("/")
-    thisFridayDashes += splitDay[2]
-    thisFridayDashes += "-"
-    thisFridayDashes += splitDay[0]
-    thisFridayDashes += "-"
-    thisFridayDashes += splitDay[1]
-
-    axios.get(`https://www.hebcal.com/zmanim?cfg=json&zip=11210&date=${thisFridayDashes}`)
-        .then(function (response) {
-            console.log(response.data);
-            let location = response.data.location.name
-            let date = moment(response.data["date"], "YYYY MM DD").format("LL");
-            let minchaGedola = moment(response.data["times"]["minchaGedola"], "YYYY-MM-DDTHH:mm:ss").format("h:mm A")
-            let plagHaMincha = moment(response.data["times"]["plagHaMincha"], "YYYY-MM-DDTHH:mm:ss").format("h:mm A")
-            let legibleShkia = moment(response.data["times"]["sunset"], "YYYY-MM-DDTHH:mm:ss").format("h:mm A")
-            let tzeis50min = moment(response.data["times"]["tzeit50min"], "YYYY-MM-DDTHH:mm:ss").format("h:mm A")
-            let earlyMincha = moment(response.data["times"]["plagHaMincha"]).subtract(15, "minutes").format('LT')
-            let candleLighting = moment(response.data["times"]["sunset"]).subtract(18, "minutes").format('LT')
-            console.log("Location:", location, "Date:", date, "Mincha Gedola:", minchaGedola, "Early Mincha:", earlyMincha, "Plag:", plagHaMincha, "Candle Lighting:", candleLighting, "Shkia:", legibleShkia, "Tzeis 50 Min:", tzeis50min)
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-    thisFridayDashes = ""
-    thisFridaySlashes = moment(thisFridaySlashes).add(7, "day").format("L")
+        axios.get(`https://www.hebcal.com/zmanim?cfg=json&zip=${inputVal}&date=${thisFridayDashes}`)
+            .then(function (response) {
+                console.log(response.data);
+                let location = response.data.location.name
+                let date = moment(response.data["date"], "YYYY MM DD").format("LL");
+                let minchaGedola = moment(response.data["times"]["minchaGedola"], "YYYY-MM-DDTHH:mm:ss").format("h:mm A")
+                let plagHaMincha = moment(response.data["times"]["plagHaMincha"], "YYYY-MM-DDTHH:mm:ss").format("h:mm A")
+                let legibleShkia = moment(response.data["times"]["sunset"], "YYYY-MM-DDTHH:mm:ss").format("h:mm A")
+                let tzeis50min = moment(response.data["times"]["tzeit50min"], "YYYY-MM-DDTHH:mm:ss").format("h:mm A")
+                let earlyMincha = moment(response.data["times"]["plagHaMincha"]).subtract(15, "minutes").format('LT')
+                let candleLighting = moment(response.data["times"]["sunset"]).subtract(18, "minutes").format('LT')
+                console.log("Location:", location, "Date:", date, "Mincha Gedola:", minchaGedola, "Early Mincha:", earlyMincha, "Plag:", plagHaMincha, "Candle Lighting:", candleLighting, "Shkia:", legibleShkia, "Tzeis 50 Min:", tzeis50min)
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+        thisFridayDashes = ""
+        thisFridaySlashes = moment(thisFridaySlashes).add(7, "day").format("L")
+    }
 }
