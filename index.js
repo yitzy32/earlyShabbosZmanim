346ff7035cac513725f65206a14ddfee52fdafab
Yitzy Bratt <yitzybratt32@gmail.com>
2021-08-03 22:31:30 -0400

    create temporaryDay object

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function getInputValue() {
     let thisFridaySlashes = moment().endOf('week').subtract(1, 'day').format("L");
     let thisFridayDashes = ""
 
+    let temporaryDay = {}
     for (let index = 0; index < 4; index++) {
 
         splitDay = thisFridaySlashes.split("/")
@@ -25,6 +26,16 @@ function getInputValue() {
                 let earlyMincha = moment(response.data["times"]["plagHaMincha"]).subtract(15, "minutes").format('LT')
                 let candleLighting = moment(response.data["times"]["sunset"]).subtract(18, "minutes").format('LT')
                 console.log("Location:", location, "Date:", date, "Mincha Gedola:", minchaGedola, "Early Mincha:", earlyMincha, "Plag:", plagHaMincha, "Candle Lighting:", candleLighting, "Shkia:", legibleShkia, "Tzeis 50 Min:", tzeis50min)
+
+                temporaryDay["location"] = location
+                temporaryDay["date"] = date
+                temporaryDay["minchaGedola"] = minchaGedola
+                temporaryDay["plagHaMincha"] = plagHaMincha
+                temporaryDay["legibleShkia"] = legibleShkia
+                temporaryDay["tzeis50min"] = tzeis50min
+                temporaryDay["earlyMincha"] = earlyMincha
+                temporaryDay["candleLighting"] = candleLighting
+                console.log(temporaryDay)
             })
             .catch(function (error) {
                 console.log(error.response.data.error);
