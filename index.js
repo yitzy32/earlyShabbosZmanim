324565f2d414a4820b3e8784e73193018fbaf9f2
Yitzy Bratt <yitzybratt32@gmail.com>
2021-08-06 16:57:48 -0400

    make print button/functionality

--- index.js
+++ index.js
@@ -57,3 +57,13 @@ async function getInputValue() {
 
   tableBody.innerHTML = tbodyHmtl
 }
+
+function PrintDoc() {
+  var toPrint = document.getElementById('page-to-print');
+  var popupWin = window.open('', '_blank', 'width=900,height=500,location=no,left=200px');
+  popupWin.document.open();
+  popupWin.document.write("<html><title>::Preview::</title><link rel='stylesheet' type='text/css' href='my-css.css' /></head><body onload='window.print()'>")
+  popupWin.document.write(toPrint.innerHTML);
+  popupWin.document.write('</html>');
+  popupWin.document.close();
+}
